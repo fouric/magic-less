5921f9981d8080311d37feec8dbe40b9793b2e08
Grant Vesely <fouric@users.noreply.github.com>
2015-12-04 17:46:07 -0800

    BY THOR IT SCROLLS

--- magic_less.py
+++ magic_less.py
@@ -23,45 +23,53 @@ def tabConvert(line):
             result += line[charIndex]
             screenIndex += 1
     return result
+def drawText(t, x, y, text, fg = termbox.DEFAULT, bg = termbox.DEFAULT):
+    for i in range(len(text)):
+        t.change_cell(x + i, y, ord(text[i]), fg, bg)
 
 if __name__ == '__main__':
     lines = []
-
     for line in fileinput.input():
-        lines.append(line)
+        lines.append(line if line[-1] != '\n' else line[:-1])
 
     t = termbox.Termbox()
 
     height = t.height()
-
-    numWindows = getNumWindows(lines, height)
-
-    windowIndex = 0
-
-    windows = [[] for i in range(numWindows)]
-
     width = t.width()
 
+    numWindows = getNumWindows(lines, height)
+    windowIndex = 0
+    windows = [[] for i in range(numWindows)]
     for i in range(len(lines)):
         windows[getWindowToPutLineInto(i, height)].append(tabConvert(lines[i]))
 
-    items = []
+    barrierIndex = 0
 
     while True:
         t.clear()
         startingIndex = windowIndex * height
-        window = windows[windowIndex]
-        items.append(window[0])
-        for y in range(len(window)):
-            for x in range(min(len(window[y][:-1] if window[y][-1] == '\n' else window[y]), width)):
-                t.change_cell(x, y, ord(window[y][x]), termbox.DEFAULT, termbox.DEFAULT)
+        upper = windows[windowIndex]
+        lower = windows[windowIndex + 1 % numWindows]
+        for y in range(barrierIndex):
+            drawText(t, 0, y, lower[y])
+        drawText(t, 0, barrierIndex, '-' * width)
+        for y in range(height - barrierIndex - 1):
+            drawText(t, 0, y + barrierIndex + 1, upper[y + barrierIndex + 1])
         t.present()
         char = t.poll_event()
         if char[1] == 'q':
             break
         elif char[1] == 'j':
-            windowIndex = (windowIndex + 1) % numWindows
+            #windowIndex = (windowIndex + 1) % numWindows
+            barrierIndex += 1
+            if barrierIndex >= height:
+                windowIndex = windowIndex + 1 % numWindows
+                barrierIndex = 0
         elif char[1] == 'k':
-            windowIndex = (windowIndex - 1) % numWindows
+            #windowIndex = (windowIndex - 1) % numWindows
+            barrierIndex -= 1
+            if barrierIndex < 0:
+                windowIndex = windowIndex - 1 % numWindows
+                barrierIndex = height - 1
 
     t.close()
